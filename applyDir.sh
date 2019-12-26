401e5a3594a30bfd05661b486d337ab67d0cfa36
Jan van Brügge <supermanitu@gmail.com>
2019-12-26 16:23:51 +0100

    general: Allow apply script to delete service

--- applyDir.sh
+++ applyDir.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 dir=$1
+action="apply"
+
+if [ ! -z $2 ]; then
+    action=$2
+fi
 
 if [ ! -e "$dir/apply.sh" ]; then
     dhall-to-yaml --omit-empty --documents --file "$dir/$dir.dhall" | \
-        kubectl apply -f -
+        kubectl $action -f -
 else
     cd "$dir"
-    ./apply.sh
+    ./apply.sh $action
     cd ..
 fi
--- cert-manager/apply.sh
+++ cert-manager/apply.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+action=$1
+
 echo "$(< ./customResources.yaml)
 ---
 $(dhall-to-yaml --omit-empty --documents --file cert-manager.dhall)" | \
-    kubectl apply -f -
+    kubectl $action -f -
